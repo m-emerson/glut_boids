4941f0c0767f2bec6dfdabd349d555970f7f8844
Marisa Emerson <nin@hemmingway.local>
2014-05-17 13:44:40 +1000

    bug that looks spacey

--- src/boids.c
+++ src/boids.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
@@ -6,14 +6,7 @@
 #include <GL/glut.h>
 #include "boids.h"
 
-#define MAXBOIDS 20
-#define MAXSELECT 100
-#define MAXFEED 300
-#define	SOLID 1
-#define	LINE 2
-#define	POINT 3
-
-GLint windW = 300, windH = 300;
+GLint windW = 1000, windH = 800;
 GLuint selectBuf[MAXSELECT];
 GLfloat feedBuf[MAXFEED];
 GLint vp[4];
@@ -25,42 +18,49 @@ float mouseX, mouseY;
 GLint objectCount;
 GLint numObjects;
 
-// Vector struct
-struct vector {
-	int x;
-	int y;
-} vector;
-
-// Triangle has three vectors/points
-struct triangle {
-	struct vector p1;
-	struct vector p2;
-	struct vector p3;
-	struct vector centre;
-} triangle;
-	
-// Struct for boid
-struct boid {
-	float v1[2];
-	float v2[2];
-	float v3[2];
-	float color[3];
-	float rotate;
-
-	struct triangle t;
-	// Velocity
-
-	float velocity[2];
-
-	// Vectors for applying rules
-	float r1[2];
-	float r2[2];
-	float r3[2];
-	float r4[2];
-
-} boids[MAXBOIDS];
-
 GLenum linePoly = GL_FALSE;
+
+// Bounds for position
+int XMax = 300;
+int XMin = -300;
+int YMax = 300;
+int YMin = -300;
+
+// Adds two vectors
+struct vector
+AddVectors(struct vector v1, struct vector v2)
+{
+	struct vector ret;
+	ret.x = v1.x + v2.x;
+	ret.y = v1.x + v2.y;
+	return ret;
+}
+
+float
+square(float a)
+{
+	float ret;
+	ret = a * a;
+	return(ret);
+}
+
+int
+check_within_radius(struct vector *centre, struct vector *v)
+{
+	// c^2 = a^2 + b^2
+	if ( (square(v->x - centre->x) + square(v->y - centre->y)) < square(LOCAL_RADIUS) )
+		return 1;
+	else
+		return 0;
+}
+
+float
+GetMagnitude(struct vector *v)
+{
+	float t;
+	t = v->x*v->x + v->y*v->y;
+	return sqrt(t);	
+}
 
 static void
 InitObjects(int num)
@@ -81,35 +81,33 @@ InitObjects(int num)
 		x = (rand() % 300) - 150;
 		y = (rand() % 300) - 150;
 
+		// Set the triangle variables for each boid
+
 		boids[i].t.p1.x = x;
-		boids[i].t.p2.x = x + 10;
-		boids[i].t.p3.x = x + 5;
+		printf("Setting boids[i].t.p1.x to: %f\n", boids[i].t.p1.x);
+		boids[i].t.p2.x = x + 5;
+		boids[i].t.p3.x = x + 2.5;
 		boids[i].t.p1.y = y;
 		boids[i].t.p2.y = y;
-		boids[i].t.p2.y = y + 20;
+		boids[i].t.p3.y = y + 10;
 
-		boids[i].v1[0] = x;
-		boids[i].v2[0] = x + 10;
-		boids[i].v3[0] = x + 5;
-		boids[i].v1[1] = y;
-		boids[i].v2[1] = y;
-		boids[i].v3[1] = y + 20;
 		boids[i].color[0] = ((rand() % 100) + 50) / 150.0;
 		boids[i].color[1] = ((rand() % 100) + 50) / 150.0;
 		boids[i].color[2] = ((rand() % 100) + 50) / 150.0;
 
-		boids[i].velocity[0] = 0;
-		boids[i].velocity[1] = 0;
+		boids[i].velocity.x = 0;
+		boids[i].velocity.y = 0;
 
 		// Initialise rotation angle to 0
 		boids[i].rotate = 0.0f;
 	}
+	printf("memaddress: %d\n", &boids[0]);
 }
 
 static void
 Init(void)
 {
-	numObjects = 20;
+	numObjects = 100;
 	InitObjects(numObjects);
 }
 
@@ -134,20 +132,20 @@ Render(GLenum mode)
 {
 	GLint i;
 
-	for (i = 0; i < objectCount; i++) {
+	for (i = 0; i < numObjects; i++) {
 		if (mode == GL_SELECT) {
 			glLoadName(i);
 		}
 		//glRotatef(boids[i].rotate, 0.0f, 0.0f, 0.5f);
 		glColor3fv(boids[i].color);
 		glBegin(GL_POLYGON);
-		glVertex2fv(boids[i].v1);
-		glVertex2fv(boids[i].v2);
-		glVertex2fv(boids[i].v3);
+		glVertex2f(boids[i].t.p1.x, boids[i].t.p1.y);
+		glVertex2f(boids[i].t.p2.x, boids[i].t.p2.y);
+		glVertex2f(boids[i].t.p3.x, boids[i].t.p3.y);
 		glEnd();
 
 		// Make the fish rotate
-		boids[i].rotate += 1.0f;
+//		boids[i].rotate += 1.0f;
 	}
 }
 
@@ -241,7 +239,6 @@ MoveMouse(int x, int y)
 static void
 AvoidPredator()
 {
-	printf("%f\n", mouseX);
 	int i;
 	for (i = 0; i < MAXBOIDS; i++) {
 		// Begin avoiding if the mouse only if the vectors are close
@@ -270,9 +267,33 @@ Mouse(int button, int state, int mouseX, int mouseY)
 }
 
 static void
+DrawSlider(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);  
+	/*glColor3f(0.0,0.4,0.2); 
+	glPointSize(3.0);  
+
+	glBegin(GL_LINES);
+		glVertex2i(0.1f,0.1f);
+		glVertex2i(0.75f,-0.75f);
+	glEnd();*/
+
+
+       glColor3f(0.0f, 0.0f, 0.0f);
+       glRectf(-0.75f,0.75f, 0.75f, -0.75f);
+       glutSwapBuffers();
+
+}
+
+static void
 Draw(void)
 {
 	int i;
+
+	struct vector *r0;
+	struct vector *r1;
+	struct vector *r2;
+	struct vector *r3;
 
 	glPushMatrix();
 	glMatrixMode(GL_PROJECTION);
@@ -286,29 +307,36 @@ Draw(void)
 	glRotatef(zRotation, 0, 0, 1);
 
 	// R1	
-	MoveTowardsCentre();
-
-	// R2
-	KeepDistance();
-
-	// R3
-	MatchVelocity();
-
-	// R4
-	AvoidPredator();
 
 	for (i = 0; i < MAXBOIDS; i++) {
+		
+		// Apply Rules
+		r0 = StayInBounds(&boids[i]);
+		r1 = MoveTowardsCentre(&boids[i]);
+		r2 = MatchVelocity(&boids[i]);
+		r3 = LimitSpeed(&boids[i]);
 
-		boids[i].velocity[0] = boids[i].velocity[0] + boids[i].r1[0] + boids[i].r2[0] + boids[i].r3[0] + boids[i].r4[0];
-		boids[i].velocity[1] = boids[i].velocity[1] + boids[i].r1[1] + boids[i].r2[1] + boids[i].r3[0] + boids[i].r4[1];
-		boids[i].v1[0] = boids[i].v1[0] + boids[i].velocity[0];
-		boids[i].v1[1] = boids[i].v1[1] + boids[i].velocity[1];
-		boids[i].v2[0] = boids[i].v2[0] + boids[i].velocity[0];
-		boids[i].v2[1] = boids[i].v2[1] + boids[i].velocity[1];
-		boids[i].v3[0] = boids[i].v3[0] + boids[i].velocity[0];
-		boids[i].v3[1] = boids[i].v3[1] + boids[i].velocity[1];
+		// Add R0 to velocity
+		VectorAdd(&boids[i].velocity, &boids[i].velocity, r0);
+		// Add R1 to velocity
+		VectorAdd(&boids[i].velocity, &boids[i].velocity, r1);
+		// Add R2 to velocity
+		VectorAdd(&boids[i].velocity, &boids[i].velocity, r2);
+		
+		if (boids[i].velocity.y > 1000) {
+			exit(1);
+		}
+	
+		// Apply velocity to position	
+		VectorAdd(&boids[i].t.p1, &boids[i].t.p1, &boids[i].velocity);
+		VectorAdd(&boids[i].t.p2, &boids[i].t.p2, &boids[i].velocity);
+		VectorAdd(&boids[i].t.p3, &boids[i].t.p3, &boids[i].velocity);
+
+		free(r0);
+		free(r1);
 	}
 
+	DrawSlider();
 	Render(GL_RENDER);
 	glPopMatrix();
 	glutSwapBuffers();
@@ -413,9 +441,11 @@ DoFeedback(void)
 	glPopMatrix();
 
 	x = glRenderMode(GL_RENDER);
+
 	if (x == -1) {
 		x = MAXFEED;
 	}
+
 	DrawFeedback((GLint) x);
 }
 
@@ -468,37 +498,104 @@ SpecialKey(int key, int x, int y)
 	}
 }
 
-// Rule 1
 void
-MoveTowardsCentre(void)
+VectorAdd(struct vector *ret, struct vector *a, struct vector *b)
 {
-	int i, j;
-	/* Move all the boids towards the perceived centre */
+	ret->x = a->x + b->x;
+	ret->y = a->y + b->y;
+}
+
+void
+VectorMinus(struct vector *ret, struct vector *a, struct vector *b)
+{
+	ret->x = a->x - b->x;
+	ret->y = a->y - b->y;
+}
+
+void
+VectorDivide(struct vector *ret, struct vector *a, float scalar)
+{
+
+	ret->x = a->x / scalar;
+	ret->y = a->y / scalar;
+}
+
+struct vector *
+StayInBounds(struct boid *b)
+{
+	struct vector *v = malloc(sizeof(struct vector));
+
+	// Initialise values to 0
+	v->x = 0;
+	v->y = 0;
+
+	//printf("t.p1.x: %f\n", b->t.p1.x);	
+	
+	if (b->t.p1.x < (float)XMin)
+		v->x = 10;
+	else if (b->t.p1.x > (float)XMax)
+		v->x = -10;
+	
+	if (b->t.p1.y < (float)YMin)
+		v->y = 10;
+	else if (b->t.p1.y > (float)YMax) 
+		v->y = -10;
+
+	return v;
+}
+
+struct vector *
+LimitSpeed(struct boid *b)
+{
+	struct vector *v = malloc(sizeof(struct vector));
+
+	if (GetMagnitude(v) > MAX_SPEED) {
+		VectorDivide(&b->velocity, &b->velocity, GetMagnitude(v));
+		b->velocity.x = b->velocity.x * (float)MAX_SPEED;
+		b->velocity.y = b->velocity.y * (float)MAX_SPEED;
+	}
+}
+
+// Rule 1
+struct vector *
+MoveTowardsCentre(struct boid *b)
+{
+	struct vector *v = malloc(sizeof(struct vector));
+	int i, j; 
+
+	int boids_count = 0; // Count of boids in local radius
+
+	v->x = 0;
+	v->y = 0;
 
 	for (i = 0; i < MAXBOIDS; i++) {
-		// Initialise to 0
-		boids[i].r1[0] = 0;
-		boids[i].r1[1] = 0;
-
-		for (j = 0; j < MAXBOIDS; j++) {
-			// Skip the positional information of the boid we're looking at
-			if (i == j)
-				continue;
-
-			// In future we may want to set the 'position' of the boid as it's centre, but for now let's just pick one point
-			boids[i].r1[0] += boids[j].v1[0];
-			boids[i].r1[1] += boids[j].v1[1];
+		// Check if its the same boid (memory addresses are the same)
+		// If it is, skip it
+		if (b == &boids[i])
+			continue;
+		
+		if (check_within_radius(&b->t.p1, &boids[i].t.p1)) { 
+			//printf("It is within the correct radius\n");
+			VectorAdd(v, v, &boids[i].t.p1);
+			boids_count++;
 		}
-
-		// Calculate vector offset
-		// pcj = pcj / n - 1
-		boids[i].r1[0] = boids[i].r1[0] / (MAXBOIDS - 1);
-		boids[i].r1[1] = boids[i].r1[0] / (MAXBOIDS - 1);
-
-		// 1% towards the centre = pcj = bj.position / 100 
-		boids[i].r1[0] = (boids[i].r1[0] - boids[i].v1[0]) / 100;
-		boids[i].r1[1] = (boids[i].r1[1] - boids[i].v1[1]) / 100;
+		
 	}
+	
+	// Nothing in the local radius
+	if (v->x == 0 && v->y == 0)
+		return v;
+
+	// Calculate vector offset
+	printf("Boids count: %d\n", boids_count);
+	VectorDivide(v, v, (float)(boids_count));
+	//VectorDivide(v, v, (float)(MAXBOIDS - 1));
+	// 1% towards the centre = pcj = bj.position / 100
+	VectorMinus(v, v, &boids[i].t.p1);	
+	VectorDivide(v, v, 100);
+	printf("The calculation yielded: x:%f, y:%f\n", v->x, v->y);
+	
+	return v; 
 }
 
 // Rule 2
@@ -529,26 +626,37 @@ KeepDistance(void)
 }
 
 // Rule 3
-static void
-MatchVelocity(void)
+struct vector *
+MatchVelocity(struct boid *b)
 {
 	int i, j;
+	struct vector *v = malloc(sizeof(struct vector));
+
+	int boids_count = 0;
+	v->x = 0;
+	v->y = 0;
+
 	for (i = 0; i < MAXBOIDS; i++) {
-		for (j = 0; j < MAXBOIDS; j++) {
-			if (i == j)
-				continue;
-			boids[i].r3[0] = boids[i].r3[0] + boids[j].velocity[0];
-			boids[i].r3[1] = boids[i].r3[1] + boids[j].velocity[1];
-		}	
-		// Calcualte perceived velocity
-		boids[i].r3[0] = boids[i].r3[0] / (MAXBOIDS - 1);
-		boids[i].r3[1] = boids[i].r3[1] / (MAXBOIDS - 1);
+		if (b == &boids[i])
+			continue;
+
+		if (check_within_radius(&b->t.p1, &boids[i].t.p1)) {
+			VectorAdd(v, v, &boids[i].velocity);
+			boids_count++;
+		}
+	}	
+
+	if (!boids_count)
+		return v;
 		
-		// Add a portion to the current velocity (lets say 1/8th)
-		boids[i].r3[0] = (boids[i].r3[0] - boids[i].velocity[0]) / 20;
-		boids[i].r3[1] = (boids[i].r3[1] - boids[i].velocity[1]) / 20;
-		
-	}
+	// Calcualte perceived velocity
+	VectorDivide(v, v, boids_count);
+	
+	// Add a portion to the current velocity (lets say 1/8th)
+	VectorMinus(v, v, &b->velocity);
+	VectorDivide(v, v, 8.0);	
+
+	return v;
 }
 
 static void
@@ -559,7 +667,6 @@ DetermineNewPositions(void)
 	   pcj = b1.position + b2.position + ... + bj-1.position + bj+1.position + ... + bn.position) / (N-1) */
 
 	// Apply rule 1
-
 }
 
 int
@@ -567,7 +674,8 @@ main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-	glutCreateWindow("Select Test");
+	glutInitWindowSize(windW, windH);
+	glutCreateWindow("OpenGL Boids");
 	Init();
 	glutReshapeFunc(Reshape);
 	glutKeyboardFunc(Key);
--- src/boids.h
+++ src/boids.h
@@ -1,4 +1,47 @@
+#define MAXBOIDS 100
+#define MAXSELECT 100
+#define MAXFEED 300
+#define SOLID 1
+#define LINE 2
+#define POINT 3
+#define LOCAL_RADIUS 50
+#define MAX_SPEED 3
+
+struct vector {
+	float x;
+	float y;
+} vector;
+
+struct triangle {
+	struct vector p1;
+	struct vector p2;
+	struct vector p3;
+	struct vector centre;
+} triangle;
+
+struct boid {
+	struct triangle t;
+	struct vector velocity;
+
+	float v1[2];
+	float v2[2];
+	float v3[2];
+
+	float color[3];
+	float rotate;
+
+	float r1[2];
+	float r2[2];
+	float r3[2];
+	float r4[2];	
+} boids[MAXBOIDS];
+
 static void Timer(int value);
-static void MoveTowardsCentre(void);
+struct vector * MoveTowardsCentre(struct boid *b);
+struct vector * LimitSpeed(struct boid *b);
 static void KeepDistance(void);
-static void MatchVelocity(void);
+struct vector * MatchVelocity(struct boid *b);
+void VectorAdd(struct vector *ret, struct vector *a, struct vector *b);
+void VectorDivide(struct vector *ret, struct vector *a, float scalar);
+struct vector * StayInBounds(struct boid *b);
+int check_within_radius(struct vector *centre, struct vector *v);
